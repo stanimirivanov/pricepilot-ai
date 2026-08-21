55f338cf7658cdf432c65f35e3a792ceff9118fb
Stanimir Ivanov <stanimir.ivanov@gmail.com>
2026-08-21 08:52:42 +0300

    [#28] Integrate demand forecasts with elasticity model (#30)
    Co-authored-by: Stanimir Ivanov <stanimr@protonmail.com>

---
+++ src/pricepilot/models/__init__.py
@@ -0,0 +1,6 @@
+"""Models for dynamic pricing"""
+
+from pricepilot.models.elasticity import PriceElasticityModel
+from pricepilot.models.forecast_pricing import ForecastPricingModel
+
+__all__ = ["PriceElasticityModel", "ForecastPricingModel"]
