022ea599e129cca9c898b54ca030d061acd9e9bb
Stanimir Ivanov <stanimr@protonmail.com>
2026-08-21 11:47:46 +0200

    [#31] Implement anomaly detection for demand patterns

--- src/pricepilot/models/__init__.py
+++ src/pricepilot/models/__init__.py
@@ -1,6 +1,11 @@
 """Models for dynamic pricing"""
 
+from pricepilot.models.anomaly_detector import DemandAnomalyDetector
 from pricepilot.models.elasticity import PriceElasticityModel
 from pricepilot.models.forecast_pricing import ForecastPricingModel
 
-__all__ = ["PriceElasticityModel", "ForecastPricingModel"]
+__all__ = [
+    "PriceElasticityModel",
+    "ForecastPricingModel",
+    "DemandAnomalyDetector",
+]
